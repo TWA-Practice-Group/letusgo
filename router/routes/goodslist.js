ccbc63bec9e700d2d4acf9f8d5ad0a5ec6d6811c
hawang <654296217@qq.com>
2015-03-05 00:31:54 +0800

    rename goodsList to helloWorld

--- router/routes/goodslist.js
+++ router/routes/goodslist.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var helloWorld = require('./modules/helloWorld.js');
 
 router.get('/', function(req, res) {
-  res.send("hello world!");
+    res.send(helloWorld());
 
 });
 
