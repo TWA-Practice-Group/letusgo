a64026c50735c8c6f383cafe52ecbc0603104d92
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-06 00:05:18 +0800

    fix conflict in addGoods.js

--- public/javascripts/addGoods.js
+++ public/javascripts/addGoods.js
@@ -1,41 +1,42 @@
 require.config({
     baseUrl: './',
     paths:{
-        'jquery': './jquery/dist/jquery',
+        'jquery': './jquery/dist/jquery'
     }
 });
 
-require(['jquery'], function($, _){
+require(['jquery'], function($) {
 
-    $(document).ready(function(){
+    $(document).ready(function () {
 
         $('#emptyError').hide();
 
         $('a#cancel').on('click', function(){
             $(this).attr('href', 'shopManagement');
+        $('a#cancel').on('click', function () {
+            $(this).attr('href', 'shopManagement');
         });
 
-        $('a#save').on('click', function(){
+        $('a#save').on('click', function () {
             verifyInfo();
         });
 
-
-        function verifyInfo(){
-            var name = $('input#goodName').val();
-            var unit = $('input#goodUnit').val();
-            var price = $('input#goodPrice').val();
-
-            var intergrated = name && unit && price;
-
-            if (!intergrated) {
-                $('#emptyError').show();
-            } else {
-
-                $.post('/api/goods', {name: name, unit: unit, price: price}, function () {
-                    $('#emptyError').hide();
-                    $('a#save').attr('href', 'shopManagement');
-                });
-            }
-        }
     });
 });
+    function verifyInfo() {
+        var name = $('input#goodName').val();
+        var unit = $('input#goodUnit').val();
+        var price = $('input#goodPrice').val();
+
+        var intergrated = name && unit && price;
+
+        if (!intergrated) {
+            $('#emptyError').show();
+        } else {
+
+            $.post('/api/goods', {name: name, unit: unit, price: price}, function () {
+                $('#emptyError').hide();
+                $('a#save').attr('href', 'shopManagement');
+            });
+        }
+    }
