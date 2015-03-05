350337c6e27ef572e871263d9bfecfb8d0808037
hawang <654296217@qq.com>
2015-03-05 11:54:00 +0800

    modify config path

--- public/javascripts/addGoods.js
+++ public/javascripts/addGoods.js
@@ -2,8 +2,8 @@ var require = require('require.js');
 
 require.cofig({
     paths:{
-        'jquery': '../../../bower_components/jquery/dist/jquery.js',
-        'semantic': '../../../bower_components/semantic-ui/dist/semantic.js'
+        'jquery': '../../bower_components/jquery/dist/jquery.js',
+        'semantic': '../../bower_components/semantic-ui/dist/semantic.js'
     }
 });
 require(['jquery', 'semantic'], function (jquery, semantic){
