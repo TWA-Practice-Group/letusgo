8ab7a623dc590e212c9344045e768210a51b5a52
hawang <654296217@qq.com>
2015-03-05 18:14:08 +0800

    add require.js

--- public/javascripts/addGoods.js
+++ public/javascripts/addGoods.js
@@ -1,51 +1,41 @@
-//require.config({
-//    baseUrl: '../../'
-//    //paths:{
-//    //    'jquery': '../bower_components/jquery/dist'
-//    //}
-//});
-
-//require(['jquery/dist/jquery'], function($){
-//
-//    $("document").ready(function(){
-//
-//        $('a#cancel').on('click', function(){
-//            $(this).attr('href', '../../views/shopManagement.html');
-//        });
-//
-//        $('a#save').on('click', savaGood());
-//    });
-//});
-
-$(document).ready(function(){
-
-    $('#emptyError').hide();
-
-    $('a#cancel').on('click', function(){
-        $(this).attr('href', '../../views/shopManagement.html');
-    });
-
-    $('a#save').on('click', function(){
-        verifyInfo();
-    });
-
-    function verifyInfo(){
-        var name = $('input#goodName').val();
-        var unit = $('input#goodUnit').val();
-        var price = $('input#goodPrice').val();
-
-        var intergrated = name && unit && price;
-
-        if(!intergrated){
-            $('#emptyError').show();
-        }else{
-            console.log(name+unit+price);
-            $.post('/api/goods', {name:, unit:, price:}, function(){
-
-                $('#emptyError').hide();
-                $('a#save').attr('href', '../../views/shopManagement.html');
-            });
-        }
+require.config({
+    baseUrl: './',
+    paths:{
+        'jquery': './jquery/dist/jquery',
     }
 });
 
+require(['jquery'], function($, _){
+
+    $(document).ready(function(){
+
+        $('#emptyError').hide();
+
+        $('a#cancel').on('click', function(){
+            $(this).attr('href', '../../views/shopManagement.html');
+        });
+
+        $('a#save').on('click', function(){
+            verifyInfo();
+        });
+
+
+        function verifyInfo(){
+            var name = $('input#goodName').val();
+            var unit = $('input#goodUnit').val();
+            var price = $('input#goodPrice').val();
+
+            var intergrated = name && unit && price;
+
+            if(!intergrated){
+                $('#emptyError').show();
+            }else{
+
+                $.post('/api/goods', {name: name, unit: unit, price: price}, function(){
+                    $('#emptyError').hide();
+                    $('a#save').attr('href', '../../views/shopManagement.html');
+                });
+            }
+        }
+    });
+});
