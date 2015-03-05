6d52a09060ef6f08af9597980320e2c793be7779
hawang <654296217@qq.com>
2015-03-05 20:30:38 +0800

    finish post and get method

--- router/routes/goods.js
+++ router/routes/goods.js
@@ -2,18 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var Good = require('./model/goodModel');
+var goodModel = require('./modules/goodsSchema');
 
 router.get('/', function(req, res) {
-    res.send('hello world!');
+    res.send(goodModel.get());
 
 });
 
 router.post('/', function(req, res){
-    var good = new Good(null, req.body.name, req.body.unit, req.body.price);
-
-    console.log(req.body.name, req.body.unit, req.body.price);
-    console.log(JSON.stringify(req.param('name')));
-
+    goodModel.create(req.body);
+    res.send('add successful');
 });
 
 module.exports = router;
