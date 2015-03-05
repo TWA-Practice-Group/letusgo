c82592c99721a5942a220531b54e60ab643efbe6
hawang <654296217@qq.com>
2015-03-05 17:35:19 +0800

    add listener

--- public/javascripts/addGoods.js
+++ public/javascripts/addGoods.js
@@ -1,11 +1,51 @@
-var require = require('require.js');
+//require.config({
+//    baseUrl: '../../'
+//    //paths:{
+//    //    'jquery': '../bower_components/jquery/dist'
+//    //}
+//});
 
-require.cofig({
-    paths:{
-        'jquery': '../../bower_components/jquery/dist/jquery.js',
-        'semantic': '../../bower_components/semantic-ui/dist/semantic.js'
+//require(['jquery/dist/jquery'], function($){
+//
+//    $("document").ready(function(){
+//
+//        $('a#cancel').on('click', function(){
+//            $(this).attr('href', '../../views/shopManagement.html');
+//        });
+//
+//        $('a#save').on('click', savaGood());
+//    });
+//});
+
+$(document).ready(function(){
+
+    $('#emptyError').hide();
+
+    $('a#cancel').on('click', function(){
+        $(this).attr('href', '../../views/shopManagement.html');
+    });
+
+    $('a#save').on('click', function(){
+        verifyInfo();
+    });
+
+    function verifyInfo(){
+        var name = $('input#goodName').val();
+        var unit = $('input#goodUnit').val();
+        var price = $('input#goodPrice').val();
+
+        var intergrated = name && unit && price;
+
+        if(!intergrated){
+            $('#emptyError').show();
+        }else{
+            console.log(name+unit+price);
+            $.post('/api/goods', {name:, unit:, price:}, function(){
+
+                $('#emptyError').hide();
+                $('a#save').attr('href', '../../views/shopManagement.html');
+            });
+        }
     }
 });
-require(['jquery', 'semantic'], function (jquery, semantic){
 
-});
