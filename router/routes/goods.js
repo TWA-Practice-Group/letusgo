1e3725e8a0e3f090460b0eba9b2372310f78f361
hawang <654296217@qq.com>
2015-03-05 20:23:11 +0800

    add id to goodModel

--- router/routes/goods.js
+++ router/routes/goods.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var Good = require('./model/goodModel');
+
 router.get('/', function(req, res) {
     res.send('hello world!');
 
 });
 
 router.post('/', function(req, res){
+    var good = new Good(null, req.body.name, req.body.unit, req.body.price);
 
+    console.log(req.body.name, req.body.unit, req.body.price);
+    console.log(JSON.stringify(req.param('name')));
 
 });
 
