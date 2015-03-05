cfe691d449d1b2af0aa53821abf00b298746609e
hawang <654296217@qq.com>
2015-03-05 11:13:46 +0800

    add addGoods route

--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
     app.use('/api/goodslist', require('./routes/goodslist'));
     app.use('/', require('./routes/home'));
+    app.use('/addGoods', require('./routes/addGoods'));
 };
