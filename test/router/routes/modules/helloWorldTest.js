7a9a065cc062ee62802c1d72ba1e8890a5b33ca3
hawang <654296217@qq.com>
2015-03-05 09:54:56 +0800

    add require

--- test/router/routes/modules/helloWorldTest.js
+++ test/router/routes/modules/helloWorldTest.js
@@ -1,9 +1,16 @@
 'use strict';
 
+var chai = require('chai');
+global.expect = chai.expect;
+
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+var baseUrl = '../../../../router/routes/modules';
+
 describe('helloWorld', function(){
     it('should return hello world!', function(){
-
-        var helloWorld = require('helloWorld');
+        var helloWorld = require(baseUrl + '/helloWorld');
         expect(helloWorld()).to.equal('hello world!');
     });
 });
