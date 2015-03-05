57c3b931d58978e7de39a48df57ad152cfd16305
hawang <654296217@qq.com>
2015-03-05 18:40:38 +0800

    solve conflict in index.js

--- router/index.js
+++ router/index.js
@@ -3,4 +3,5 @@ module.exports = function(app) {
     app.use('/shop', require('./routes/shop'));
     app.use('/', require('./routes/home'));
     app.use('/shopManagement', require('./routes/shopManagement'));
+    app.use('/addGoods', require('./routes/addGoods'));
 };
