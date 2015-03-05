aaa406394029dceac95985bd50fac5e74e7618b4
hawang <654296217@qq.com>
2015-03-06 00:40:45 +0800

    finish cart.js to render vart.jade

--- router/routes/cart.js
+++ router/routes/cart.js
@@ -1,3 +1,13 @@
-/**
- * Created by hgwang on 3/6/15.
- */
+var express = require('express');
+var router = express.Router();
+
+var cartSchema = require('./modules/cartSchema.js');
+
+router.get('/', function(req, res, next) {
+    cartSchema.find(function (err, cartGoods) {
+        if (err) return next(err);
+        res.render('cart',{cart:cartGoods});
+    });
+});
+
+module.exports = router;
