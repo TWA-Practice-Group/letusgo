b1a8ccfefde83f8238eb2b626fc1b09d62309d93
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-05 17:37:05 +0800

    modify database connection part

--- router/routes/shopManagement.js
+++ router/routes/shopManagement.js
@@ -1,18 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-
-var goods = mongoose.model('goods', { name: String, price: Number, unit: String });
-
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-    if (err) // ...
-        console.log('meow');
-});
-
-
 router.get('/', function(req, res) {
     //res.send("hello world!");
     res.render('shopManagement');
