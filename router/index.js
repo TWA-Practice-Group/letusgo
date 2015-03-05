fbaea339d402d59862140d80ca9dea38941a8a90
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-05 10:23:14 +0800

    add shop route

--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
-    app.use('/api/goodslist', require('./routes/goodslist'));
+    app.use('/api/shop', require('./routes/shop'));
     app.use('/', require('./routes/home'));
+    app.use('/shopManagement', require('./routes/shopManagement'));
 };
