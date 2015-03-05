e93463cd3803811bd250822d0a12d7abaadd6f4f
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-05 17:52:17 +0800

    complete read database

--- router/routes/shopManagement.js
+++ router/routes/shopManagement.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    //res.send("hello world!");
-    res.render('shopManagement');
-});
+var mongoose = require('mongoose');
+var connect = require('./modules/goodsSchema.js');
 
+router.get('/', function(req, res, next) {
+
+    connect.find(function (err, goods) {
+
+        if (err) return next(err);
+        res.render('shopManagement',{goods: goods});
+    });
+});
 
 module.exports = router;
