55a288767e60af2aec7c108819207559f5fabf96
hawang <654296217@qq.com>
2015-03-05 11:17:12 +0800

    add require.js to addGoods.js

--- public/javascripts/addGoods.js
+++ public/javascripts/addGoods.js
@@ -4,7 +4,6 @@ require.cofig({
     paths:{
         'jquery': '../../../bower_components/jquery/dist/jquery.js',
         'semantic': '../../../bower_components/semantic-ui/dist/semantic.js'
-
     }
 });
 require(['jquery', 'semantic'], function (jquery, semantic){
