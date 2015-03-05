792345b7844b87b0b63dd06cb9abee46d1eafee4
hawang <654296217@qq.com>
2015-03-05 21:50:04 +0800

    correct get method of goods

--- router/routes/goods.js
+++ router/routes/goods.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-var Good = require('./model/goodModel');
-var goodModel = require('./modules/goodsSchema');
+var goodModel = require('./modules/goodsSchema.js');
 
 router.get('/', function(req, res) {
-    res.send(goodModel.get());
 
+    goodModel.find(function(err, goods){
+
+        if (err) return next(err);
+        res.send(JSON.parse(goods));
+    });
 });
 
 router.post('/', function(req, res){
+
     goodModel.create(req.body);
     res.send('add successful');
 });
