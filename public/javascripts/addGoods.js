ee5bc171a94d0d2e2d95130785f5f106ce12c2e9
hawang <654296217@qq.com>
2015-03-05 11:14:31 +0800

    add addGoods.js

--- public/javascripts/addGoods.js
+++ public/javascripts/addGoods.js
@@ -1,3 +1,12 @@
-/**
- * Created by hgwang on 3/5/15.
- */
+var require = require('require.js');
+
+require.cofig({
+    paths:{
+        'jquery': '../../../bower_components/jquery/dist/jquery.js',
+        'semantic': '../../../bower_components/semantic-ui/dist/semantic.js'
+
+    }
+});
+require(['jquery', 'semantic'], function (jquery, semantic){
+
+});
