1ecdcba89e8dd6524016e08f1c7f199a9a65358b
fgcui <fgcui1204@163.com>
2015-03-05 16:25:18 +0800

    modify shop router

--- router/index.js
+++ router/index.js
@@ -1,4 +1,4 @@
 module.exports = function(app) {
-    app.use('/api/shop', require('./routes/shop'));
+    app.use('/shop', require('./routes/shop'));
     app.use('/', require('./routes/home'));
 };
