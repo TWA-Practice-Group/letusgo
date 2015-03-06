77b44ec5e99baf60b932c9616352377553fab2e2
fgcui <fgcui1204@163.com>
2015-03-07 01:01:26 +0800

    format code

--- public/javascripts/addToCart.js
+++ public/javascripts/addToCart.js
@@ -1,27 +1,27 @@
 'use strict';
 require.config({
-    baseUrl: './',
-    paths:{
-        'jquery': './jquery/dist/jquery',
-        'semantic': './semantic-ui/dist/semantic'
-    }
+  baseUrl: './',
+  paths: {
+    'jquery': './jquery/dist/jquery',
+    'semantic': './semantic-ui/dist/semantic'
+  }
 });
 
-require(['jquery', 'semantic'], function($) {
+require(['jquery', 'semantic'], function ($) {
 
-    $(document).ready(function () {
+  $(document).ready(function () {
 
-        $('.buy').on('click', function () {
-            var id = this.id;
+    $('.buy').on('click', function () {
+      var id = this.id;
 
-            $.ajax({
-                url: '/api/cartGoods/',
-                type: 'POST',
-                data: {id: id},
-                success: function(result) {
-                    // Do something with the result
-                }
-            });
-        });
+      $.ajax({
+        url: '/api/cartGoods/',
+        type: 'POST',
+        data: {id: id},
+        success: function (result) {
+          // Do something with the result
+        }
+      });
     });
+  });
 });
