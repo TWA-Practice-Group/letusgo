77e2f1473f0967879882f8ef1f480b9ed83be724
fgcui <fgcui1204@163.com>
2015-03-06 21:01:07 +0800

    format code

--- router/routes/shop.js
+++ router/routes/shop.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var connect = require('./modules/goodsSchema.js');
 
-router.get('/', function(req, res, next) {
-    connect.find(function (err, goods) {
-        if (err) return next(err);
-        res.render('shop',{goods:goods});
-    });
+router.get('/', function (req, res, next) {
+  connect.find(function (err, goods) {
+    if (err) return next(err);
+    res.render('shop', {goods: goods});
+  });
 });
 
 module.exports = router;
