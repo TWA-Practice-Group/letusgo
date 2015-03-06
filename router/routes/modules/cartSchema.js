caaf6a9d4c4ffe70d3fcbe03c4c972511819dc31
fgcui <fgcui1204@163.com>
2015-03-06 12:23:22 +0800

    merage conflict

--- router/routes/modules/cartSchema.js
+++ router/routes/modules/cartSchema.js
@@ -1,11 +1,4 @@
-'use strict';
-var mongoose = require('mongoose');
+/**
+ * Created by fgcui on 3/6/15.
+ */
 
-var cartSchema = new mongoose.Schema({
-    name: String,
-    unit: String,
-    price: Number,
-    number: Number
-});
-
-module.exports = mongoose.model('cart', cartSchema);
