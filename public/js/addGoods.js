fabcbd6bf2344508e3c828f49af11100419b56ad
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-06 20:56:52 +0800

    post request correct

--- public/js/addGoods.js
+++ public/js/addGoods.js
@@ -1,66 +1,60 @@
 'use strict';
 
 require.config({
-    baseUrl: './',
-    paths:{
-        'jquery': './jquery/dist/jquery',
-        'semantic': './semantic-ui/dist/semantic'
-    }
+  baseUrl: './',
+  paths:{
+    'jquery': './jquery/dist/jquery',
+    'semantic': './semantic-ui/dist/semantic'
+  }
 });
 
-require(['jquery', 'semantic'], function($, semantic) {
-    $(document).ready(function(){
+require(['jquery', 'semantic'], function ($) {
 
+  $(document).ready(function () {
+
+    $('#emptyError').hide();
+    $('#priceError').hide();
+
+    $('a#save').on('click', verifyInfo);
+
+    function verifyInfo(){
+      var unit = $('input#goodUnit').val();
+      var price = $('input#goodPrice').val();
+      var name = $('input#goodName').val();
+
+      var isIntergrated = name && unit && price;
+
+      if (!isIntergrated) {
+        $('#emptyError').show();
+      } else {
         $('#emptyError').hide();
+        priceIsNumber(name, unit, price);
+      }
+    }
+
+    function priceIsNumber(name, unit, price){
+
+      var reg = /^\d+(\.\d+)?$/;
+
+      var  priceIsNumber = reg.exec(price);
+
+      if(!priceIsNumber){
+
+        $('#priceError').show();
+      }else{
+
         $('#priceError').hide();
+        saveNewGood(name, unit, price);
+      }
+    }
 
-        $('a#cancel').on('click', function () {
+    function saveNewGood(name, unit, price){
 
-            $(this).attr('href', '/shopManagement');
+      $.post('/api/goods', {name: name, unit: unit, price: price})
+        .success(function(){
+          $(location).attr('href','/shopManagement')
         });
-        $('a#save').on('click', function () {
-            verifyInfo();
-        });
-    });
+    }
 
+  });
 });
-    function verifyInfo() {
-        var name = $('input#goodName').val();
-        var unit = $('input#goodUnit').val();
-        var price = $('input#goodPrice').val();
-
-
-        var isIntergrated = name && unit && price;
-
-        if (!isIntergrated) {
-            $('#emptyError').show();
-        } else {
-
-            priceIsNumber(name, unit, price);
-                $('#emptyError').hide();
-                priceIsNumber(name, unit, price);
-            }
-        if(!priceIsNumber){
-            $('#emptyError').show();
-        }else{
-            saveNewGood(name, unit, price);
-            if(!priceIsNumber){
-
-                $('#priceError').show();
-            }else{
-
-                $('#priceError').hide();
-                saveNewGood(name, unit, price);
-            }
-        }
-    }
-
-    function saveNewGood() {
-
-        $.post('/api/goods', {name: name, unit: unit, price: price})
-            .success(function () {
-
-                $('#emptyError').hide();
-                $('a#save').attr('href', '/shopManagement');
-            });
-    }
