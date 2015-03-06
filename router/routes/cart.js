0b0a425990430e1c8a46f769e4f78d9742912459
fgcui <fgcui1204@163.com>
2015-03-06 21:00:40 +0800

    format code

--- router/routes/cart.js
+++ router/routes/cart.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 var cartSchema = require('./modules/cartSchema.js');
 
-router.get('/', function(req, res, next) {
-    cartSchema.find(function (err, cartGoods) {
-        if (err) return next(err);
-        res.render('cart',{cart:cartGoods});
-    });
+router.get('/', function (req, res, next) {
+  cartSchema.find(function (err, cartGoods) {
+    if (err) return next(err);
+    res.render('cart', {cart: cartGoods});
+  });
 });
 
 module.exports = router;
