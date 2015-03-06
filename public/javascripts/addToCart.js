0966b84f6c5c7d2504e31667a23dd7cda6f1b78e
fgcui <fgcui1204@163.com>
2015-03-06 20:53:49 +0800

    add onclick event

--- public/javascripts/addToCart.js
+++ public/javascripts/addToCart.js
@@ -2,19 +2,26 @@
 require.config({
     baseUrl: './',
     paths:{
+        'jquery': './jquery/dist/jquery',
+        'semantic': './semantic-ui/dist/semantic'
     }
 });
 
-require(['jquery', 'semantic'], function($, ui ) {
+require(['jquery', 'semantic'], function($) {
 
     $(document).ready(function () {
 
         $('.buy').on('click', function () {
-            addToCart();
+            var id = this.id;
+
+            $.ajax({
+                url: '/api/cartGoods/',
+                type: 'POST',
+                data: {id: id},
+                success: function(result) {
+                    // Do something with the result
+                }
+            });
         });
-
-        function addToCart(){
-
-        }
     });
 });
