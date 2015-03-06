f539e16ff8aeea710add8aee32c4c7eddb7f50a7
hawang <654296217@qq.com>
2015-03-06 11:41:56 +0800

    solve conflict in addGoods

--- public/javascripts/addGoods.js
+++ public/javascripts/addGoods.js
@@ -19,27 +19,29 @@ require(['jquery', 'semantic'], function ($, semantic) {
 
                 $(this).attr('href', '/shopManagement');
             });
-            $('a#save').on('click', function(){
+            $('a#save').on('click', function () {
                 verifyInfo();
             });
 
-        });
-    });
+            function verifyInfo() {
+                var name = $('input#goodName').val();
+                var unit = $('input#goodUnit').val();
+                var price = $('input#goodPrice').val();
+
+                var isIntergrated = name && unit && price;
+
+                if (!isIntergrated) {
+                    $('#emptyError').show();
+                } else {
+
+                    $.post('/api/goods', {name: name, unit: unit, price: price}, function () {
+                        $('#emptyError').hide();
+                        $('a#save').attr('href', '/shopManagement');
+                    });
+                }
+
+            }
+        })
+    })
 });
-    function verifyInfo() {
-        var name = $('input#goodName').val();
-        var unit = $('input#goodUnit').val();
-        var price = $('input#goodPrice').val();
 
-        var intergrated = name && unit && price;
-
-        if (!intergrated) {
-            $('#emptyError').show();
-        } else {
-
-            $.post('/api/goods', {name: name, unit: unit, price: price}, function () {
-                $('#emptyError').hide();
-                $('a#save').attr('href', '/shopManagement');
-            });
-        }
-    }
