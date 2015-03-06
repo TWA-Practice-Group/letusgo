aabb5728e80b686fc6c49ec87bb2857e6487bc91
yfwang <yufei930101@gmail.com>
2015-03-06 13:47:14 +0800

    delete code

--- router/routes/modifyGoods.js
+++ router/routes/modifyGoods.js
@@ -3,19 +3,11 @@ var router = express.Router();
 
 var goodModel = require('./modules/goodsSchema.js');
 
-//router.get('/:id', function(req, res) {
-//    var id = req.params.id;
-//    goodModel.findById(id, function(err, goods){
-//        if (err) return next(err);
-//        res.render('modifyGoods', {goods:goods});
-//    });
-//});
-
-
 router.get('/', function(req, res, next) {
     goodModel.find(function (err, goods) {
         if (err) return next(err);
         res.render('modifyGoods',{goods:goods});
     });
 });
+
 module.exports = router;
