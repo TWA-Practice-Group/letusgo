3865cc46bf344a4bd7a1a8fe5af4581a89ec9b8f
fgcui <fgcui1204@163.com>
2015-03-06 20:52:00 +0800

    add cart init number

--- router/routes/cartGoods.js
+++ router/routes/cartGoods.js
@@ -1,36 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-var Cart = require('./modules/cartSchema.js');
+var Cart = require('./modules/cartSchema');
 var goods = require('./modules/goodsSchema');
-
-//
-Cart.find(function () {
-
-    }).populate('goods')
-    .exec(function (err, data) {
-        console.log('cart');
-    });
-router.get('/', function (req, res) {
-
-    Cart.find(function (err, cart) {
-
-        if (err) return next(err);
-        res.send(cart);
-    });
-});
 
 router.post('/', function (req, res) {
 
-    Cart.create(req.body);
-    res.send('add successful');
+
+    var id = req.body.id;
+   Cart.create({goodsId: id, number: 2});
+
+   res.send('add successful');
 });
 
-function add(err) {
-    if (err) throw err;
-    Cart.goods.push(goods._id);
-    number: 1;
-    lucy.save(cb1);
-}
 module.exports = router;
 
