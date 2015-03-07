f4b4e1b8c93f16762e4847841603e5d33b868e87
fgcui <fgcui1204@163.com>
2015-03-07 13:00:19 +0800

    fix conflicts in modifyGoods.js

--- router/routes/modifyGoods.js
+++ router/routes/modifyGoods.js
@@ -1,11 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
-var goodModel = require('./modules/goodsSchema.js');
-
-router.get('/', function(req, res) {
-    res.render('modifyGoods');
-});
-
-
-module.exports = router;
+/**
+ * Created by hgwang on 3/5/15.
+ */
