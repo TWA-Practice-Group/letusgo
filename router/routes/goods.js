6689a04f60b6f0efc2201137cdfdb21e4862c2c2
fgcui <fgcui1204@163.com>
2015-03-07 12:59:26 +0800

    fix conflicts in goods.js

--- router/routes/goods.js
+++ router/routes/goods.js
@@ -1,28 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var Good = require('./model/goodModel');
-var goodModel = require('./modules/goodsSchema');
+var goodModel = require('./modules/goodsSchema.js');
 
 router.get('/', function(req, res) {
 
-    res.send(goodModel.get());
-
     goodModel.find(function(err, goods){
+
         if (err) return next(err);
-        res.send(goods);
-    });
-});
-
-router.post('/', function(req, res) {
-
-    var good = new Good(null, req.body.name, req.body.unit,req.body.price);
-    good.postGood();
-
-    res.send({
-        status: 200,
-        info: 'add successful',
-        data: {}
+        res.send(JSON.parse(goods));
     });
 });
 
