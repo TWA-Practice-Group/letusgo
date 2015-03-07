4c26a11493e5037acab3c01e73e62d058e0b962f
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-07 15:52:50 +0800

    add functions to goodsSchema

--- router/route/model/goodsSchema.js
+++ router/route/model/goodsSchema.js
@@ -1,6 +1,7 @@
 'use strict';
-
+var _ = require('lodash');
 var mongoose = require('mongoose');
+
 var goodsSchema = new mongoose.Schema({
 
   name: String,
@@ -8,4 +9,41 @@ var goodsSchema = new mongoose.Schema({
   price: Number
 });
 
-module.exports = mongoose.model('goods', goodsSchema);
+var goodsModel = mongoose.model('goods', goodsSchema);
+
+goodsModel.postGood = function(name, unit, price) {
+
+  goodHasexisted(name, unit, price);
+};
+
+function addNewGood(name, unit, price) {
+
+  goodsModel.create({
+    name: name,
+    price: price,
+    unit: unit
+  });
+}
+
+function goodHasexisted(name, unit, price) {
+
+  goodsModel.find(function (err, goods) {
+    if (err) return next(err);
+
+    var goodindex = _.findIndex(goods, function (eachGood) {
+      return eachGood.name == name;
+    });
+
+    if (goodindex === -1) {
+      addNewGood(name, unit, price);
+    }
+  });
+}
+module.exports = goodsModel;
+//module.exports = mongoose.model('goods', goodsSchema);
+
+
+
+
+
+
