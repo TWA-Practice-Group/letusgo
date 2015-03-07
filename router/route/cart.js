78518fef78af51bffb614e36acb22a849dc19ee6
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-07 13:32:56 +0800

    rename routes to route

---
+++ router/route/cart.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var cartSchema = require('./modules/cartSchema.js');
+
+router.get('/', function (req, res, next) {
+  cartSchema.find(function (err, cartGoods) {
+    if (err) return next(err);
+    res.render('cart', {cart: cartGoods});
+  });
+});
+
+router.post('/', function (req, res) {
+
+  cartModel.create(req.body);
+  res.send('add successful');
+});
+
+module.exports = router;
