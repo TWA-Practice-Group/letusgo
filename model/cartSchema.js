5e28e1a98f2af0728b4813ca6a1ae794ccefe047
fgcui <fgcui1204@163.com>
2015-03-09 23:42:37 +0800

    modify cart Schema

--- model/cartSchema.js
+++ model/cartSchema.js
@@ -1,11 +1,13 @@
 'use strict';
 var mongoose = require('mongoose');
+var Goods = require('./goodsSchema');
 
 var cartSchema = new mongoose.Schema({
-  name: String,
-  unit: String,
-  price: Number,
-  number: Number
+  goodsId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Goods'
+  },
+  number:Number
 });
 
 module.exports = mongoose.model('cart', cartSchema);
