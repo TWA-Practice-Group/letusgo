49402f13d774880589270879fc4bfbed149d59fb
yfwang <yufei930101@gmail.com>
2015-03-09 15:45:50 +0800

    update modifyGoods.js

--- router/route/modifyGoods.js
+++ router/route/modifyGoods.js
@@ -3,23 +3,15 @@ var router = express.Router();
 
 var goodModel = require('../../model/goodsSchema.js');
 
-router.get('/', function (req, res) {
-  res.render('modifyGoods');
-});
+router.get('/:id', function (req, res, next) {
 
-router.get('/:id', function (req, res) {
   var id = req.params.id;
+
   goodModel.findById(id, function (err, goods) {
-    if (err) {
-      res.status(500).end();
-    }
-    if (goods == null) {
-      res.status(400).end();
-    }
-    else {
-      res.send(goods);
-    }
+    if(err) return next(err);
+    res.render('modifyGoods', {good:goods});
   })
+  
 });
 
 module.exports = router;
