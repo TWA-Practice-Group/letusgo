8570257be7516d9bfe94a6d992ad4f65b8da25d5
yfwang <yufei930101@gmail.com>
2015-03-09 15:43:16 +0800

    add $location of modifyGoods button

--- public/js/shopManagement.js
+++ public/js/shopManagement.js
@@ -29,5 +29,10 @@ require(['semantic', 'jquery'], function (semantic, $) {
         }
       });
     });
+
+    $('.modifyGoods').on('click', function () {
+      var id = this.id;
+      $(location).attr('href', '/modifyGoods/' + id);
+    })
   });
 });
