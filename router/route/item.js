012b23bf994d39aa2e1569205465625eb08b7826
yfwang <yufei930101@gmail.com>
2015-03-10 11:39:52 +0800

    move method to ItemSchema.js

--- router/route/item.js
+++ router/route/item.js
@@ -51,11 +51,7 @@ router.post('/:id', function (req, res) {
   var id = req.params.id;
   var result;
 
-  Item.update({_id: id}, {$set:
-  {name: req.body.name,
-   unit: req.body.unit,
-   price: req.body.price}
-  }, function(err) {
+  Item.updateItem(id, req.body.name, req.body.unit, req.body.price, function (err) {
     if (err) {
       result = {
         status: 400
@@ -70,7 +66,6 @@ router.post('/:id', function (req, res) {
 
     res.send(result);
   });
-
 });
 
 module.exports = router;
