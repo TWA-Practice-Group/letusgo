a53a62cb66e9cf255b4344b20d1fc86e0b420090
yfwang <yufei930101@gmail.com>
2015-03-10 12:27:19 +0800

    rename ItemSchema to itemSchema

--- router/route/shop.js
+++ router/route/shop.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var connect = require('../../model/ItemSchema.js');
+var connect = require('../../model/itemSchema.js');
 
 router.get('/', function (req, res, next) {
   connect.find(function (err, items) {
