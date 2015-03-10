307efee104e4fed9ab9cffc1cb3e461b6ec776c6
fgcui <fgcui1204@163.com>
2015-03-10 09:43:05 +0800

    modify cartGoods find

--- router/route/cartItem.js
+++ router/route/cartItem.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-var Cart = require('../../model/cartSchema.js');
-
+var Cart = require('../../model/cartSchema');
+var Goods = require('../../model/goodsSchema');
 router.get('/', function (req, res) {
 
-  Cart.find(function (err, cart) {
+  Cart.find()
+    .populate('goodsId', 'name unit price', null)
+    .exec(function(err, docs) {
+      console.log('099999999');
+      console.log(docs[0].goodsId.name); // post-by-aikin
+      res.send(docs[0].goodsId.name);
+    });
 
-    if (err) return next(err);
-    res.send(cart);
-  });
+  //Cart.find(function (err, cart) {
+  //
+  //  if (err) return next(err);
+  //  res.send(cart);
+  //});
 });
 
 router.post('/', function (req, res) {
@@ -18,4 +26,3 @@ router.post('/', function (req, res) {
 });
 
 module.exports = router;
-
