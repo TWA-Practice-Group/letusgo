aebab8baf21c2f3969c37ad433cd011d81ba9846
fgcui <fgcui1204@163.com>
2015-03-10 14:21:06 +0800

    modify find

--- router/route/cartItem.js
+++ router/route/cartItem.js
@@ -3,15 +3,24 @@ var router = express.Router();
 
 var Cart = require('../../model/cartSchema');
 var Item = require('../../model/ItemSchema');
-router.get('/', function (req, res) {
+//router.get('/', function (req, res) {
+//
+//  Cart.find()
+//    .populate('itemId', 'name unit price', null)
+//    .exec(function(err, cartItem) {
+//      res.render('cart',{cartItems:cartItem});
+//    });
+//});
 
-  Cart.find()
-    .populate('itemId', 'name unit price', null)
-    .exec(function(err, cartItem) {
-      res.render('cart',{cartItems:cartItem});
-    });
-});
-
+//router.get('/:id', function(req, res){
+//  var id = req.params.id;
+//
+//  Cart.findById(id, function (err, cartItem) {
+//    if(cartItem != null){
+//      cartItem.number += 1;
+//    }
+//  })
+//});
 router.post('/', function (req) {
   var id = req.body.id;
   Cart.create({itemId: id, number: 1});
