20d138f3e79a5292b5d7f30df2a769b17511a038
fgcui <fgcui1204@163.com>
2015-03-10 11:06:51 +0800

    remove goods model

--- router/route/cartItem.js
+++ router/route/cartItem.js
@@ -2,16 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var Cart = require('../../model/cartSchema');
-var Goods = require('../../model/goodsSchema');
 router.get('/', function (req, res) {
-
-  Cart.find()
-    .populate('goodsId', 'name unit price', null)
-    .exec(function(err, docs) {
-      console.log('099999999');
-      console.log(docs[0].goodsId.name); // post-by-aikin
-      res.send(docs[0].goodsId.name);
-    });
+  //
+  //Cart.find()
+  //  .populate('goodsId', 'name unit price', null)
+  //  .exec(function(err, docs) {
+  //    console.log('099999999');
+  //    console.log(docs[0].goodsId.name); // post-by-aikin
+  //    res.send(docs[0].goodsId.name);
+  //  });
 
   //Cart.find(function (err, cart) {
   //
